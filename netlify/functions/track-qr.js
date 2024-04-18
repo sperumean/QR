4f8a10f298be4c99c9b75ef9e64c6aaa74b4f1aa
sperumean <sperumean@calbaptist.edu>
2024-04-17 17:16:51 -0700

    Your commit message

--- netlify/functions/track-qr.js
+++ netlify/functions/track-qr.js
@@ -6,6 +6,8 @@ const connection = mysql.createConnection({
   user: 'steven',
   password: 'Spiderman57#',
   database: 'qrcode'
+  connectTimeout: 60000, // Increase the timeout to 60 seconds (60000 ms)
+
 });
 
 function generateRandomString(length) {
@@ -25,31 +27,41 @@ exports.handler = async (event) => {
   console.log('Connecting to MySQL database...');
 
   // Connect to the MySQL database
-  connection.connect();
-
-  console.log('Connected to MySQL database. Inserting scan record...');
-
-  // Insert a new record into the qr_scans table
-  connection.query(
-    'INSERT INTO qr_scans (timestamp, request_id) VALUES (?, ?)',
-    [timestamp, requestId],
-    (error, results) => {
-      if (error) {
-        console.error('Error inserting scan log:', error);
-      } else {
-        console.log('QR Code scanned at:', timestamp, 'Request ID:', requestId);
-      }
+  connection.connect((err) => {
+    if (err) {
+      console.error('Error connecting to MySQL database:', err);
+      // Optionally, you can return an error response here
+      return {
+        statusCode: 500,
+        body: 'Error connecting to MySQL database',
+      };
     }
-  );
 
-  // Close the database connection
-  connection.end();
+    console.log('Connected to MySQL database. Inserting scan record...');
+
+    // Insert a new record into the qr_scans table
+    connection.query(
+      'INSERT INTO qr_scans (timestamp, request_id) VALUES (?, ?)',
+      [timestamp, requestId],
+      (error, results) => {
+        if (error) {
+          console.error('Error inserting scan log:', error);
+        } else {
+          console.log('Scan record inserted successfully');
+          console.log('QR Code scanned at:', timestamp, 'Request ID:', requestId);
+        }
+
+        // Close the database connection
+        connection.end();
+      }
+    );
+  });
 
   // Redirect scanners to a different site
   return {
     statusCode: 301,
     headers: {
-      Location: 'https://www.calbaptist.edu', // Replace with the desired redirect URL
+      Location: 'https://www.calbaptist.edu',
     },
   };
 };
